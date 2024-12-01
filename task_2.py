317d9616e3c995b32bc01ec0365946e5fd904d35
Olena <ivanova.olenk@gmail.com>
2024-12-01 09:59:15 -0800

    add checks for range, and uniqueness of winning numbers, add sort for winning numbers

--- task_2.py
+++ task_2.py
@@ -3,13 +3,20 @@ from random import randint
 def get_numbers_ticket(min, max, quantity):
     win_tickets = []
     try:
-        for _ in range(quantity):
-            win_tickets.append(randint(min, max))
+        if max-min < quantity:
+            return "Please enter a valid range. Not enough unique numbers available."
+    
+        while len(win_tickets) < quantity:
+            num = randint(min, max)
+            if num not in win_tickets:
+                win_tickets.append(num)
     except TypeError:
-        print("Please enter a valid integer.")
+        return "Please enter a valid integer."
     else:
-        return f"Ваші виграшні числа: {win_tickets}"
+        win_tickets.sort()
+        return f"Winning numbers: {win_tickets}"
 
+print(get_numbers_ticket(9, 10, 3)) 
 print(get_numbers_ticket(1, 10, 3)) 
 print(get_numbers_ticket(1, 10, "three")) 
 print(get_numbers_ticket("1", 10, 3)) 
